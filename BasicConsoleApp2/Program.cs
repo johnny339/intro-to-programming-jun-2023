94506f6189c742a6d9a5e3c5bcbc2763740ddd71
Johnny Guzman <guzmanj774@gmail.com>
2023-06-20 16:54:27 -0400

    update bank app2

--- BasicConsoleApp2/Program.cs
+++ BasicConsoleApp2/Program.cs
@@ -1,4 +1,9 @@
-﻿// // See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore;
 
-var builder = new WebApplicatoin.CreateBuilder
+var builder = new WebApplication.CreateBuilder(args);
+
+
+
+var app = builder.Build();
+
+app.Run();
