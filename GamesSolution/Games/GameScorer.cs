1f3427c557ad053046a9a28613fa3443de6859ad
Johnny Guzman <guzmanj774@gmail.com>
2023-06-21 16:50:00 -0400

    update for wedensday building a bowling game and doing tests on it. Also added some golf tests.

--- GamesSolution/Games/GameScorer.cs
+++ GamesSolution/Games/GameScorer.cs
@@ -3,12 +3,27 @@
 public class GameScorer
 {
 
+    // Overloading
+    public ScoreReport GenerateScoreReportFor(GolfGame game)
+    {
+        GuardAgainstInvalidGame(game);
+        var players = game.GetPlayers();
+
+        int highScore = players.Max(p => p.score);
+        int lowScore = players.Min(p => p.score);
+
+
+        return new ScoreReport
+        {
+            Winners = players.Where(p => p.score == lowScore).ToList(),
+            Losers = highScore == lowScore ? new() : players.Where(p => p.score == highScore).ToList(),
+            Average = players.Average(p => p.score)
+        };
+    }
+
     public ScoreReport GenerateScoreReportFor(BowlingGame game)
     {
-        // Pre conditions - all of the scores are in the valid ranges and there are no duplicate names.
-        // Winners, Losers, Average
-
-        // To Find the winners....
+        GuardAgainstInvalidGame(game);
         var players = game.GetPlayers();
 
         int highScore = players.Max(p => p.score);
@@ -18,8 +33,16 @@ public class GameScorer
         return new ScoreReport
         {
             Winners = players.Where(p => p.score == highScore).ToList(),
-            Losers = players.Where(p => p.score == lowScore).ToList(),
+            Losers = highScore == lowScore ? new() : players.Where(p => p.score == lowScore).ToList(),
             Average = players.Average(p => p.score)
         };
+    }
+
+    private static void GuardAgainstInvalidGame(Game game)
+    {
+        if (game.GetPlayers().Count < 2)
+        {
+            throw new InvalidGameException();
+        }
     }
 }
