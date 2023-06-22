a4c1e686cad87d5f7a9a91886a303119dfa64efa
Johnny Guzman <guzmanj774@gmail.com>
2023-06-22 16:56:12 -0400

    part 2 of string calculator

--- StringCalculatorThursday/StringCalculator.cs
+++ StringCalculatorThursday/StringCalculator.cs
@@ -4,8 +4,29 @@ namespace StringCalculator;
 public class StringCalculator
 {
 
+    private readonly ILogger _logger;
+
+    public StringCalculator(ILogger logger)
+    {
+        _logger = logger;
+    }
+
     public int Add(string numbers)
     {
-        return -42;
+        int result = 0;
+
+        if (numbers == "") { result = 0; }
+        else
+        {
+
+            result = numbers.Split(',', '\n').Select(int.Parse).Sum();
+        }
+        _logger.Write(result.ToString());
+        return result;
     }
+}
+
+public interface ILogger
+{
+    void Write(string message);
 }
