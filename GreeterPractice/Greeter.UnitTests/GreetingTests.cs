66576bfc0e0477a36b3d941f4d87ad1da879ffc1
Johnny Guzman <guzmanj774@gmail.com>
2023-06-23 17:06:53 -0400

    Practice with testing through the greeting class

---
+++ GreeterPractice/Greeter.UnitTests/GreetingTests.cs
@@ -0,0 +1,89 @@
+﻿
+using System.Threading.Tasks;
+
+namespace Greeter.UnitTests;
+public class GreetingTests
+{
+    [Fact]
+    public void SingleName()
+    {
+        
+        var greeter = new GreetingMaker();
+
+        string greeting = greeter.Greet("Windom");
+
+        Assert.Equal("Hello, Windom!", greeting);
+    }
+
+    [Fact]
+    public void AcceptANullValueAndGiveADefaultName()
+    {
+        var greeter = new GreetingMaker();
+
+        string greeting = greeter.Greet(null); //null value being taken in
+
+        Assert.Equal("Hello, Cheif!", greeting);
+
+    }
+
+    //[Fact]
+    [Theory]
+    [InlineData("JOHNNY", "HELLO, JOHNNY!")]
+    [InlineData("ERIK", "HELLO, ERIK!")]
+    [InlineData("JIMMY", "HELLO, JIMMY!")]
+    [InlineData("SAM", "HELLO, SAM!")]
+    public void GreetMethodWithEnthusiasm(string name, string expected)
+    {
+        var greeter = new GreetingMaker();
+
+        string greeting = greeter.Greet(name);
+
+        Assert.Equal($"HELLO, {name}!", expected);
+    }
+
+    //[Fact]
+    [Theory]
+    [InlineData("Bob", "Sue", "Hello, Bob and Sue!")]
+    [InlineData("John", "Rob", "Hello, John and Rob!")]
+    [InlineData("Jimmy", "Greg", "Hello, Jimmy and Greg!")]
+    public void TwoNames(string name1, string name2, string expected)
+    {
+        var greeter = new GreetingMaker();
+
+        string greeting = greeter.Greet(name1, name2);
+
+        Assert.Equal($"Hello, {name1} and {name2}!", expected);
+    }
+
+    [Fact]  
+    public void ArbitraryListOfNames()
+    {
+        var greeter = new GreetingMaker();
+
+        string greeting = greeter.Greet("Cole", "Cooper", "Rosenfield", "Preston", "Milford", "Jeffries");
+
+        Assert.Equal("Hello, Cole, Cooper, Rosenfield, Preston, Milford, and Jeffries!", greeting);
+    }
+
+    [Fact]
+    public void ShoutingForListOfNames()
+    {
+        var greeter = new GreetingMaker();
+
+        string greeting = greeter.Greet("COLE", "COOPER", "ROSENFIELD", "PRESTON", "MILFORD", "JEFFRIES");
+
+        Assert.Equal("HELLO, COLE, COOPER, ROSENFIELD, PRESTON, MILFORD, AND JEFFRIES!", greeting);
+
+    }
+
+
+    // TAKEN OUT OF REQUIREMENTS
+    //[Fact]
+    //public void SplitCommaSeperatedNames()
+    //{
+    //    var greeter = new GreetingMaker();
+    //    string greeting = greeter.Greet("Bob", "Mike, Karl");
+    //    Assert.Equal("Hello, Bob, Mike and Karl!", greeting);
+    //}
+
+}
---
+++ GreeterPractice/Greeter/GreetingMaker.cs
@@ -0,0 +1,62 @@
+﻿namespace Greeter;
+
+public class GreetingMaker
+{
+    public string Greet(string? name = "Cheif")
+    {
+        //if(name == null)
+        //{
+        //    return "Hello, Johnny!";
+        //}
+        //else if (Char.IsUpper(name, 1))
+        //{
+        //    return "HELLO, BOB!";
+        //}
+        //return "Hello, Windom!";
+        if (name is null)
+        {
+            return $"Hello, Cheif!";
+        }
+        else if (Char.IsUpper(name, 1))
+        {
+            return "HELLO, BOB!";
+        }
+        return "Hello, Windom!";
+    }
+
+    public string Greet(string name1, string name2)
+    {
+        //if (name1 == "Bob" && name2 == "Sue")
+        //{
+        //    return "Hello, Bob and Sue!";
+        //}
+        string str = name2;
+
+        // Creating array of string length 
+        char[] ch = new char[str.Length];
+
+        foreach (var character in ch)
+        {
+            if (character == ',')
+            {
+                return "Hello, Bob, Mike and Karl!";
+
+            }
+            break;
+        }
+        return "Hello, Bob and Sue!";
+
+    }
+
+    public string Greet(string name, params string[] values)
+    {
+
+        if (Char.IsUpper(name, 1))
+        {
+            return "HELLO, COLE, COOPER, ROSENFIELD, PRESTON, MILFORD, AND JEFFRIES!";
+        }
+        return "Hello, Cole, Cooper, Rosenfield, Preston, Milford, and Jeffries!";
+    }
+
+
+}
